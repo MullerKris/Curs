6b49af67fb2ce6d4b5c9f1425ab4fe7c89393d90
MullerKris <mullerkris1991@gmail.com>
2017-08-01 01:00:29 -0700

    -calculator

--- tema2.php
+++ tema2.php
@@ -1,23 +1,21 @@
 <?php
-session_start();
   if(isset($_POST['color'])) {
       $color=$_POST['color'];
   }
   else {
-	  $color= "white";
+	  $color= "black";
   }
 ?>
 <html>
 	<head>
 		<body bgcolor="<?php echo $color; ?>">
-
 <form  method="post">
 <select name="color">
-		<option style="background-color: white">Select color</option>
+		<option value="white" >Select color</option>
         <option style="background-color: green" >Green</option>
         <option style="background-color: blue" >Blue</option>
         <option style="background-color: red" >Red</option>
-		<option style="background-color: black" >Black</option>
+		<option style="background-color: black"  >Black</option>
 		<option style="background-color: yellow" >Yellow</option>
 		<option style="background-color: grey" >Grey</option>
 		<option style="background-color: purple" >Purple</option>
