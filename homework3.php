b74e618c093ce605b8c7f67e4ffa76019a13e72b
MullerKris <mullerkris1991@gmail.com>
2017-08-01 13:49:00 -0700

    Change background color every 4 clicks

--- homework3.php
+++ homework3.php
@@ -1,23 +1,34 @@
 
 <?php
- if (isset($_POST['culoare'])){
-      $culoare=$_POST['culoare'];
+if (isset($_POST['color']))
+	$color=$_POST['color'];
+else
+	$color="#FFFFFF";
+
+if (isset($_POST['counter'])) {
+	$counter = $_POST['counter'] + 1;
+	$counter = $counter % 4;
 }
-$a=array(1,2,3,4,5,6,7,8,9,'a','b','c','d','f');
-$culoare= "#";
-for($i=1;$i<=6;$i++)
-	$culoare.=$a[rand(0,15)];
+else
+	$counter = 1;
+
+if ($counter == 0) {
+	$a=array(0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f');
+	$color= "#";
+	for($i=1;$i<=6;$i++)
+		$color.=$a[rand(0,15)];
+}
 ?>
 
 <html>
 	<head>
-
-
-		<body style="background: <?php echo $culoare; ?>;">
-	<form method="post">
-		<input type="submit"  value="Background-Color" />
-		<input type="hidden"  />
-	</form>
+		<body style="background: <?php echo $color; ?>;">
+	
+			<form method="post">
+				<input type="submit"  value="Background-Color" />
+				<input type="hidden" name="counter" value="<?php echo $counter; ?>" /> 
+				<input type="hidden" name="color" value="<?php echo $color; ?>" />
+			</form>
 
 		</body>
 	</head>
