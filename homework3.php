a7c6a60123b23279b1c38889065c8faab5e8d868
MullerKris <mullerkris1991@gmail.com>
2017-08-05 05:54:53 -0700

    Change code from $counter

--- homework3.php
+++ homework3.php
@@ -7,12 +7,11 @@ else
 
 if (isset($_POST['counter'])) {
 	$counter = $_POST['counter'] + 1;
-	$counter = $counter % 4;
+
 }
 else
 	$counter = 1;
-
-if ($counter == 0) {
+if ($counter % 4 == 0) {
 	$a=array(0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f');
 	$color= "#";
 	for($i=1;$i<=6;$i++)
