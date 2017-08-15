2f2da944d6a9db7113f24b9706d0d306fa66bc24
MullerKris <mullerkris1991@gmail.com>
2017-08-15 02:26:45 -0700

    -Added second exercise

---
+++ homework5.php
@@ -0,0 +1,47 @@
+<?php 
+
+if (isset($_POST['trimite'])) {
+	if (empty($_POST['nume'])) echo "scrie ceva la nume!";
+	elseif (empty($_POST['prenume'])) echo "scrie ceva la prenume!";
+	elseif (empty($_POST['adresa'])) echo "scrie ceva la adresa!";
+	else {
+		//pasul 1. creez o denumire dinamica de fisier
+		$fisier = "useri.txt";
+		$handle = fopen($fisier, "a+");
+		flock($handle, LOCK_EX);
+		flock($handle, LOCK_UN);
+		fclose($handle);
+}
+		//pasul 2. creez o poveste cu tot ce s-a completat in formular		
+		$poveste = "Nume: ".$_POST['nume']."\r\nPrenume: ".$_POST['prenume']."\r\nAdresa: ".str_ireplace("\r\n", "\r\n\t", $_POST['adresa'])."\r\nData nasterii: ".$_POST['zi']."/".$_POST['luna']."/".$_POST['an']."\r\nFumator: ".$_POST['fumator']."\r\n**********\r\n";
+		
+		//pasul 3. scriu povestea in fisier
+		$handle = fopen($fisier, "a+");
+		fwrite($handle, $poveste);
+		fclose($handle);
+		
+		
+	
+}
+?>
+<form method="post" >
+<input type="text" name="nume" placeholder="numele" /><br>
+<input type="text" name="prenume" placeholder="prenumele"  /><br>
+<textarea name="adresa" placeholder="adresa" ></textarea><br>
+<select name="zi">
+<option value="0">zi</option>
+<?php for ($i=1; $i<=31; $i++) echo "<option>$i</option>"; ?>
+</select>
+<select name="luna">
+<option value="0">luna</option>
+<?php for ($i=1; $i<=12; $i++) echo "<option>$i</option>"; ?>
+</select>
+<select name="an">
+<option value="0">an</option>
+<?php for ($i=2017; $i>=1900; $i--) echo "<option>$i</option>"; ?>
+</select><br>
+<input type="radio" name="fumator" value="da"> DA 
+<input type="radio" name="fumator" value="nu" checked> NU
+<br>
+<input type="submit" name="trimite" value="Trimite">
+</form> 
