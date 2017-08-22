aaa60e2916afbcf38da7383742dabe508284fbeb
MullerKris <mullerkris1991@gmail.com>
2017-08-22 14:10:13 -0700

    -I check if I have a cookie to login from it ($_COOKIE)

--- login.php
+++ login.php
@@ -3,6 +3,13 @@ session_start();
 $u="gigel";
 $p="123";
 
+//verific daca am cookie sa fac login din ele
+if (isset($_COOKIE['u']) && $_COOKIE['u']==$u && isset($_COOKIE['p']) && $_COOKIE['p']==$p ) {
+	$_SESSION['logat']="da";
+			$_SESSION['cinee']=$_COOKIE['u'];
+			header('location: site.php');
+			die('n-a mers redirectarea!');
+}
 
 if (isset($_POST['sub'])) {
 	if (empty($_POST['nume'])) echo "scrie ceva la nume";
@@ -10,7 +17,10 @@ if (isset($_POST['sub'])) {
 	else {
 		
 		if ($_POST['nume']==$u && $_POST['parola']==$p) {
-			
+			if ($_POST['bifa']=="on") {
+				setcookie('u', $_POST['nume'], time()+20);
+				setcookie('p', md5($_POST['parola']), time()+20);				
+			}
 			$_SESSION['logat']="da";
 			$_SESSION['cinee']=$_POST['nume'];
 			header('location: site.php');
@@ -23,5 +33,6 @@ if (isset($_POST['sub'])) {
 <form method=post>
 <input name=nume type=text /><br>
 <input name=parola type=password /><br>
+<input name=bifa type=checkbox /><br>
 <input name=sub type=submit value=intra />
 </form>
